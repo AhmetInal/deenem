c601621ca53d4decb93fbbfc7687f8e825252bbb
AhmetInal <AhmetInal@users.noreply.github.com>
2021-01-24 02:10:04 +0300

    Add project files.

---
+++ FinalSon/FinalSon.cpp
@@ -0,0 +1,194 @@
+// FinalSon.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <filesystem>
+#include <string>
+#include <stack>
+#include <map>
+#include <queue>
+
+using namespace std;
+using namespace std::filesystem;
+
+stack<string> dosyaNames;
+
+int lastArrayFinder(string arrayName[]) {   //girilen arrayde bulunan en son elemanýn indexini return eder
+	string* temp;
+	temp = arrayName;
+	for (int i = 0;i < 30;++i) {
+		if (*temp == "") {
+
+			return i - 1;
+		}
+		else {
+			temp++;
+		}
+	}
+	return -1;
+}
+
+void stringParse(string inputLine, char ParsingChar, string parcaci[], int counter) { //alýnan input stringini parsing char ile böler
+	string temp;                                                        // dýsarýda tanýmlanmýs parcaci arrayinde tutar 
+	//int counter = (lastArrayFinder(parcaci)+1);
+	/*cout << "string parse iþleminin baþlayacaðý satir: ";
+	cout << (counter + 1) << endl;*/
+	string* tutturgac;
+
+	tutturgac = (parcaci);
+
+	inputLine += " ";
+	for (std::string::iterator it = inputLine.begin(); it < inputLine.end(); ++it) {
+
+		if (*it == ParsingChar) {
+
+			*tutturgac = temp;
+			tutturgac++;
+			temp.clear();
+		}
+		else if (!(*it == ParsingChar)) {
+			temp += *it;
+
+		}
+
+	}
+}
+
+
+void reverseStr(string& str)
+{
+	int n = str.length();
+
+
+	for (int i = 0; i < n / 2; i++)
+		swap(str[i], str[n - i - 1]);
+}
+string path2Name(string str) {                            //PATH ÝLE ELDE ETTÝÐÝMÝZ YOLDA BULUNAN DOSYALARIN ÝSÝMLERÝNÝN AYRILMASI         
+	string temp = "";
+	reverseStr(str);
+	for (int i = 0;i < (int)str.size();i++)
+	{
+		if (str[i] == 92)
+		{
+			break;
+		}
+		else {
+			temp += str[i];
+		}
+	}
+	reverseStr(temp);
+	return temp;
+
+}
+
+void dosyalariBul() {
+
+	path p("C:\\Users\\ahmet\\Desktop\\cppdeneme");
+	if (!is_directory(p)) {                                                                         //BÖYLE BÝR DÝZÝN VAR MI KONTROLÜ
+
+		cout << "Burada böyle bir klasör yok" << endl;
+	}
+	else {
+		cout << "bulundugunuz directory: " << absolute(p) << endl;
+		string dontWant = "FinalSon.exe";
+		for (const auto& entry : directory_iterator(absolute(p))) {                                 //DOSYA ÝSÝMLERÝNÝ BULMA
+
+		  /*  cout << entry.path().string() << endl;*/
+
+		  // dosyaNames_Path.insert({ path2Name(entry.path().string()), entry.path().string() });     //MAP ÝÇERÝSÝNE EKLEME
+			if (!(entry.path().string() == absolute(p).string() + dontWant) ) {
+				dosyaNames.push(entry.path().string());
+				cout << "bulunan dosya: " << entry.path().string() << endl;
+			}
+
+			//cout << "burada dosya var";
+		}
+	}
+}
+
+void fileOkuma(string dosyaName, string lines[]) {                    //dosyanýn ismi girilir
+														//lines dýþarýda oluþturulmuþ ve satýrlarýn tutulacaðý arrayý belirtir
+
+	string* pointerLine = lines;                        //satýrlarý tutan arrayin adresini tutar
+	ifstream Challenges(dosyaName, ios::in);
+	int i = 0;
+	if (Challenges) { //dosya açýlýp açýlmadýðý sorgusu
+		cout << "Belirlenen dosya ismi bu klasorde mevcut" << endl;
+		cout << dosyaNames.top() << endl;
+
+		dosyaNames.pop();
+		while (!Challenges.eof()) {//dosyanýn sonuna gelene kadar bütün satýrlar 
+								//okunur ve dýþarýda tanýmlý lines arrayine gönderilir
+			getline(Challenges, *lines);
+			lines++;
+		}
+		Challenges.close();
+	}
+	else {
+		cout << "dosya bulunamadi" << endl;
+	}
+}
+
+void arraybosalt(string bosaltaray[]) {
+	for (int i = 0; i < sizeof(bosaltaray); i++) {
+		bosaltaray[i] = "";
+	}
+
+}
+int main()
+{
+	string LineParcalari[100];
+	string DosyaninLineleri[100];
+	dosyalariBul();
+	
+	for (int i = 0; i < dosyaNames.size();i++) {            //BURADA i <  DOSYA SAYISI YAPMAN GEREKYÝÞRO
+		/*cout << "dosya ad: "<<dosyaNames.top() << endl*/;
+		fileOkuma(dosyaNames.top(), DosyaninLineleri);		//SEÇÝLEN SIRADAKÝ DOSYANIN SATIRLARI DOSYALÝNES ÝÇERÝSÝNE BÝR ÝNDEX OLARAK ATILDI		
+
+		for (int i = 0; i <= lastArrayFinder(DosyaninLineleri); i++) {				//bütün satýrlar burada ayrý ayrý açýlýyor, satýrýn belirlendiði nokta
+			cout << DosyaninLineleri[i] << endl;
+			stringParse(DosyaninLineleri[i], ' ',LineParcalari,i);			//line stringi alýndý ve parçalanarak lineparçalarýnýn içerisine atýldý
+
+			for (int j = 0;j <= lastArrayFinder(LineParcalari);j++) {		//satýr içerisinde parçalarda gezilen yer saðlanan yer
+				
+				if (LineParcalari[j][0] >= 65 && LineParcalari[j][0] <= 122) {
+					cout << "BURADA BÝR HARF VAR" << endl;
+				}
+				else if (LineParcalari[j][0] == 61) {	
+					cout << "ATAMA OPERATORU VAR" << endl;
+				}
+				else if (LineParcalari[j][0] == 43) {
+					cout << "TOPLAMA OPERATORU VAR" << endl;
+				}
+				else if (LineParcalari[j][0] == 42) {
+					cout << "CARPMA OPERATORU VAR" << endl;
+				}
+				else if (LineParcalari[j][0] == 45) {
+					cout << "CIKARMA OPERATORU VAR" << endl;
+				}
+				else if (LineParcalari[j][0] == 47) {
+					cout << "BOLME OPERATORU VAR" << endl;
+				}
+
+
+
+			}
+			arraybosalt(LineParcalari);
+		}
+		cout << "Sonraki dosyaya geciliyor..." << endl;
+		arraybosalt(DosyaninLineleri);
+	}
+	
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
